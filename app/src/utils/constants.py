758df3eeab1bfcfc6a7221777812a9053d81f607
kotovahel <kotovahel1234@gmail.com>
2025-07-05 03:06:26 +0300

    alfa version

---
+++ app/src/utils/constants.py
@@ -0,0 +1,30 @@
+"""All the project constants"""
+
+import logging
+from pathlib import Path
+
+# [Dirs]
+BASE_DIR = Path('../').absolute().resolve()
+DATA_DIR = BASE_DIR.joinpath('data').absolute().resolve()
+TEMP_DIR = BASE_DIR.joinpath('temp').absolute().resolve()
+
+
+# [Logs]
+LOG_DIR = TEMP_DIR.joinpath('logs/').absolute().resolve()
+LOG_FMT = '%(asctime)s [%(levelname)s]: %(name)s: %(message)s'
+LOG_LEVEL = logging.INFO
+
+# Update logs time
+# @link: https://docs.python.org/3/library/logging.handlers.html#timedrotatingfilehandler
+UPDATE_TIME = 'midnight'
+UPDATE_INTERVAL = 1
+KEEP_OLD_LOGS = 5
+
+# [Google API]
+SERVICE_ACCOUNT_CREDENTIALS = DATA_DIR.joinpath('credentials.json')
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# [Datetime]
+DATETIME_FMT = '%Y-%m-%d-%H-%M-%S'
+
+# [OTHER]
